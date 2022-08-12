33352638c69454357cc05eea0e5794c7f2ad560d
Eugene <Eugene.Nisnevich@Gmail.com>
2022-08-11 22:39:39 -0400

    added books and modified Library constructor

--- Library.cs
+++ Library.cs
@@ -8,18 +8,27 @@ namespace LibraryTerminal
 {
     public class Library
     {
-        List<Book> LibraryContents { get; set; }
+        public List<Book> LibraryContents { get; set; }
 
-        public Library(List<Book> libraryContents)
+        public Library()//(List<Book> libraryContents)
         {
-            LibraryContents = libraryContents;
+            //LibraryContents = libraryContents;
+
+            LibraryContents = new List<Book>()
+            {
+                new Book(true, "Dune", "Frank Herbert", "Sci-fi", DateTime.Now.AddDays(7).ToString("MM-dd-yy"), "Fair"),
+                new Book(true, "The Alchemist", "Paulo Coelho", "Fantasy", DateTime.Now.AddDays(7).ToString("MM-dd-yy"), "Fair"),
+                new Book(true, "Lying For Money", "Dan Davies", "True Crime", DateTime.Now.AddDays(7).ToString("MM-dd-yy"), "Fair"),
+                new Book(true, "Kafka On The Shore", "Haruki Murakami", "Magical Realism", DateTime.Now.AddDays(7).ToString("MM-dd-yy"), "Fair"),
+                new Book(true, "Black Count", "Tom Reiss", "Non-fiction", DateTime.Now.AddDays(7).ToString("MM-dd-yy"), "Fair")
+            };
         }
 
         //***methods***
-        //return a book
+        //return a book - this could also ask for the condition of the returned book and if it is a certain value the user is "fined"
         //borrow a book
         //change list order by category
         //reserve a book
-        //
+        //remove a book (should be different from return?) I think this would allow for us to keep the duedate time as is since we would be displaying all of the books currently not checked out)
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("TEST");
+﻿using LibraryTerminal;
+
+//Book One = new Book(true, "Dune", "Frank Herbert", "Sci-fi", DateTime.Now.AddDays(7).ToString("MM-dd-yy"), "Fair");
+//Book Two = new Book(true, "The Alchemist", "Paulo Coelho", "Fantasy", DateTime.Now.AddDays(7).ToString("MM-dd-yy"), "Fair");
+//Book Three = new Book(true, "Lying For Money", "Dan Davies", "True Crime", DateTime.Now.AddDays(7).ToString("MM-dd-yy"), "Fair");
+//Book Four = new Book(true, "Kafka On The Shore", "Haruki Murakami", "Magical Realism", DateTime.Now.AddDays(7).ToString("MM-dd-yy"), "Fair");
+//Book Five = new Book(true, "Black Count", "Tom Reiss", "Non-fiction", DateTime.Now.AddDays(7).ToString("MM-dd-yy"), "Fair");
+
+Library X = new Library();
+
+foreach (Book book in X.LibraryContents)
+{
+    Console.WriteLine("{0,10},{1,15},{2,20},{3,25},{4,30},{5,35}",
+    book.OnShelf,
+    book.Title,
+    book.Author,
+    book.Category,
+    book.DueDate,
+    book.Condition);
+}//formatting is off on this, eventually would be a method within Library?
+
