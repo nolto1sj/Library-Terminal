80c2865355829a0db45a96ce3c3dc45f203eb385
nolto1sj <sjnolton@gmail.com>
2022-08-11 20:51:17 -0400

    Created book and library classes

---
+++ Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryTerminal
+{
+    public class Book
+    {
+        public bool OnShelf { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Category { get; set; }
+        public string DueDate { get; set; }
+        public string Condition { get; set; }
+
+        public Book(bool onShelf, string title, string author, string category, string dueDate, string condition)
+        {
+            this.OnShelf = onShelf;
+            this.Title = title;
+            this.Author = author;
+            this.Category = category;
+            this.DueDate = dueDate;
+            this.Condition = condition;
+        }
+
+        public Book() { }
+
+
+    }
+}
---
+++ Library.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryTerminal
+{
+    public class Library
+    {
+        List<Book> LibraryContents { get; set; }
+
+        public Library(List<Book> libraryContents)
+        {
+            LibraryContents = libraryContents;
+        }
+
+        //***methods***
+        //return a book
+        //borrow a book
+        //change list order by category
+        //reserve a book
+        //
+    }
+}
