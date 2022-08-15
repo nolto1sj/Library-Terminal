a9231482cc572784b0956c2d72e012d9ffd28492
nolto1sj <sjnolton@gmail.com>
2022-08-15 19:18:52 -0400

    added console.clear and thread.sleep

--- Library.cs
+++ Library.cs
@@ -37,16 +37,21 @@ namespace LibraryTerminal
                 Thread.Sleep(1500);
                 Console.Clear();
             }
-            else Console.WriteLine("This book has already been checked out.");
-            //listOfBooks.RemoveAt(index);
-            //Should this just change a book to showing as checked out on list and then using LINQ provide the user with a list of checked out books to then check in?
-            //also due date could be blank and set once a book is checkedout (DateTime.Now.AddDays(7).ToString("MM-dd-yy"))
-            //display a list of checkedout books and then have them select one to return (similar to checkingout)
-            //if statement to account for already checked out book
+            else
+            {
+                Console.WriteLine("This book has already been checked out. Would you like to reserve it?");
+                Thread.Sleep(1500);
+                Console.Clear();
+            }
+                //listOfBooks.RemoveAt(index);
+                //Should this just change a book to showing as checked out on list and then using LINQ provide the user with a list of checked out books to then check in?
+                //also due date could be blank and set once a book is checkedout (DateTime.Now.AddDays(7).ToString("MM-dd-yy"))
+                //display a list of checkedout books and then have them select one to return (similar to checkingout)
+                //if statement to account for already checked out book
 
-        }
+            }
 
-        public Book TitleSearch(string title)
+            public Book TitleSearch(string title)
         {
             return listOfBooks.Find(x => x.Title == title);
         }
